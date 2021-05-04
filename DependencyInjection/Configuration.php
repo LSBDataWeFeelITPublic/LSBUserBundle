40e5aab63bc278dcd26babbf2550962dfbf7e34e
Krzysztof Mazur <ruzam.pl@gmail.com>
2021-05-04 12:27:27 +0200

    New configuration tree builder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,9 +19,8 @@ use LSB\UserBundle\Manager\UserManager;
 use LSB\UserBundle\Repository\UserGroupRelationRepository;
 use LSB\UserBundle\Repository\UserGroupRepository;
 use LSB\UserBundle\Repository\UserRepository;
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use LSB\UtilityBundle\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
-use LSB\UtilityBundle\DependencyInjection\BaseExtension as BE;
 
 /**
  * This is the class that validates and merges configuration from your app/config files.
@@ -42,63 +41,42 @@ class Configuration implements ConfigurationInterface
         $treeBuilder
             ->getRootNode()
             ->children()
-            ->scalarNode(BE::CONFIG_KEY_TRANSLATION_DOMAIN)->defaultValue((new \ReflectionClass(LSBUserBundle::class))->getShortName())->end()
-            ->arrayNode(BE::CONFIG_KEY_RESOURCES)
+            ->bundleTranslationDomainScalar(LSBUserBundle::class)->end()
+            ->resourcesNode()
             ->children()
 
-            // Start User
-            ->arrayNode('user')
-            ->addDefaultsIfNotSet()
-            ->children()
-            ->arrayNode(BE::CONFIG_KEY_CLASSES)
-            ->children()
-            ->scalarNode(BE::CONFIG_KEY_ENTITY)->end()
-            ->scalarNode(BE::CONFIG_KEY_INTERFACE)->defaultValue(UserInterface::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_FACTORY)->defaultValue(UserFactory::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_REPOSITORY)->defaultValue(UserRepository::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_MANAGER)->defaultValue(UserManager::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_FORM)->defaultValue(UserType::class)->end()
+            //User
+            ->resourceNode(
+                'user',
+                UserInterface::class,
+                UserFactory::class,
+                UserRepository::class,
+                UserManager::class,
+                UserType::class
+            )
             ->end()
-            ->end()
-            ->end()
-            ->end()
-            // End User
 
-            // Start UserGroup
-            ->arrayNode('user_group')
-            ->addDefaultsIfNotSet()
-            ->children()
-            ->arrayNode(BE::CONFIG_KEY_CLASSES)
-            ->children()
-            ->scalarNode(BE::CONFIG_KEY_ENTITY)->end()
-            ->scalarNode(BE::CONFIG_KEY_INTERFACE)->defaultValue(UserGroupInterface::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_FACTORY)->defaultValue(UserGroupFactory::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_REPOSITORY)->defaultValue(UserGroupRepository::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_MANAGER)->defaultValue(UserGroupManager::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_FORM)->defaultValue(UserGroupType::class)->end()
+            //UserGroup
+            ->resourceNode(
+                'user_group',
+                UserGroupInterface::class,
+                UserGroupFactory::class,
+                UserGroupRepository::class,
+                UserGroupManager::class,
+                UserGroupType::class
+            )
             ->end()
-            ->end()
-            ->end()
-            ->end()
-            // End UserGroup
 
-            // Start UserGroupRelation
-            ->arrayNode('user_group_relation')
-            ->addDefaultsIfNotSet()
-            ->children()
-            ->arrayNode(BE::CONFIG_KEY_CLASSES)
-            ->children()
-            ->scalarNode(BE::CONFIG_KEY_ENTITY)->end()
-            ->scalarNode(BE::CONFIG_KEY_INTERFACE)->defaultValue(UserGroupRelationInterface::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_FACTORY)->defaultValue(UserGroupRelationFactory::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_REPOSITORY)->defaultValue(UserGroupRelationRepository::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_MANAGER)->defaultValue(UserGroupRelationManager::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_FORM)->defaultValue(UserGroupRelationType::class)->end()
+            //UserGroupRelation
+            ->resourceNode(
+                'user_group_relation',
+                UserGroupRelationInterface::class,
+                UserGroupRelationFactory::class,
+                UserGroupRelationRepository::class,
+                UserGroupRelationManager::class,
+                UserGroupRelationType::class
+            )
             ->end()
-            ->end()
-            ->end()
-            ->end()
-            // End UserGroupRelation
 
             ->end()
             ->end()
