52d6d3dd3f888539d296d376d2f9645a93045ede
Krzysztof Mazur <ruzam.pl@gmail.com>
2021-07-30 13:38:30 +0200

    UserInterface update

--- Entity/UserInterface.php
+++ Entity/UserInterface.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace LSB\UserBundle\Entity;
 
+use LSB\ContractorBundle\Entity\ContractorInterface;
 use Symfony\Component\Security\Core\User\UserInterface as BaseUserInterface;
 
 /**
@@ -30,4 +31,16 @@ interface UserInterface extends BaseUserInterface
     public function getLastLogin(): ?\DateTime;
 
     public function setLastLogin(?\DateTime $lastLogin): self;
+
+    public function getDefaultBillingContractor(): ?ContractorInterface;
+
+    public function setDefaultBillingContractor(?ContractorInterface $defaultBillingContractor): self;
+
+    public function getDefaultDeliveryContractor(): ?ContractorInterface;
+
+    public function setDefaultDeliveryContractor(?ContractorInterface $defaultDeliveryContractor): self;
+
+    public function getDefaultRecipientContractor(): ?ContractorInterface;
+
+    public function setDefaultRecipientContractor(?ContractorInterface $defaultRecipientContractor): self;
 }
